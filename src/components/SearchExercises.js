aa4f0afd3d409810d86cd8dea66115dd421108dd
aritra <aritrachowdhury65@gmail.com>
2022-12-24 21:42:25 +0530

    Search UI Done

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -5,7 +5,50 @@ const SearchExercises = () => {
   return (
     <div>
       <Stack alignItems="center" justifyContent="center" p="20px" mt="37px">
-        Hello
+        <Typography
+          textAlign="center"
+          fontWeight={700}
+          sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+          mb="50px"
+        >
+          Awesome Exercises You <br /> Should Know
+        </Typography>
+        <Box position="relative" mb="72px">
+          <TextField
+            sx={{
+              input: {
+                fontWeight: "700",
+                border: "none",
+                outline: "none",
+                borderRadius: "4px",
+              },
+              width: { lg: "800px", sm: "300px", xs: "250px" },
+              borderRadius: "40px",
+            }}
+            height="76px"
+            // value=""
+            onChange={(e) => {
+              console.log(e.target.value);
+            }}
+            label="Search Exercises"
+            type="text"
+          />
+          <Button
+            className="search-btn"
+            sx={{
+              bgcolor: "#d97706",
+              color: "#fff",
+              textTransform: "none",
+              width: { lg: "175px", xs: "80px" },
+              fontSize: { lg: "20px", xs: "14px" },
+              height: "56px",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            Search
+          </Button>
+        </Box>
       </Stack>
     </div>
   );
