0fdb2fec089cd88bbc04e9736332093b0703ceaf
aritra <aritrachowdhury65@gmail.com>
2022-12-24 11:06:01 +0530

    Routes done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
